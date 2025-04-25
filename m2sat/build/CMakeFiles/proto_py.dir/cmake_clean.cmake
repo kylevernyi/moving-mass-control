65bdabb44ebd0dae541043baf43082d38fdab19e
kyle <vernyikyle4@gmail.com>
2025-04-25 14:46:26 -0400

    remove build dir

--- m2sat/build/CMakeFiles/proto_py.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/proto_py"
-  "telemetry_pb2.py"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/proto_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
